53304cf5632305c706e1e488f745ee9694a946cc
Joachim Hansen <skraldespand94@hotmail.com>
2022-02-09 14:39:42 +0100

    Added the rest of the alphabet

--- piping/src/app/pipes/morse.pipe.ts
+++ piping/src/app/pipes/morse.pipe.ts
@@ -17,13 +17,119 @@ export class MorsePipe implements PipeTransform {
         case "a":
           output += ".-"
           break;
+
         case "b":
           output += "-..."
           break;
+
         case "c":
           output += "-.-."
           break;
+          
+        case "d":
+          output += "-.."
+          break;
         
+        case "e":
+          output += "."
+          break;
+          
+        case "f":
+          output += "..-."
+          break;
+          
+        case "g":
+          output += "--."
+          break;
+          
+        case "h":
+          output += "...."
+          break;
+          
+        case "i":
+          output += ".."
+          break;
+          
+        case "j":
+          output += ".---"
+          break;
+          
+        case "k":
+          output += "-.-"
+          break;
+          
+        case "l":
+          output += ".-.."
+          break;
+          
+        case "m":
+          output += "--"
+          break;
+          
+        case "n":
+          output += "-."
+          break;
+          
+        case "o":
+          output += "---"
+          break;
+          
+        case "p":
+          output += ".--."
+          break;
+          
+        case "q":
+          output += "--.-"
+          break;
+          
+        case "r":
+          output += ".-."
+          break;
+          
+        case "s":
+          output += "..."
+          break;
+          
+        case "t":
+          output += "-"
+          break;
+
+        case "u":
+          output += "..-"
+          break;
+            
+        case "v":
+          output += "...-"
+          break;
+          
+        case "w":
+          output += ".--"
+          break;
+          
+        case "x":
+          output += "-..-"
+          break;
+          
+        case "y":
+          output += "-.--"
+          break;
+          
+        case "z":
+          output += "--.."
+          break;
+          
+        case "æ":
+          output += ".-.-"
+          break;
+          
+        case "ø":
+          output += "---."
+          break;
+          
+        case "å":
+          output += ".--.-"
+          break;
+          
         default:
           break;
       }
